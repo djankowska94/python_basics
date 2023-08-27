66e0f45d590e2478246a8e899bacc8e71ac7a9a7
Dorota Jankowska <dorotajankowska@MacBook-Pro-Dorota.local>
2023-08-27 14:50:16 +0200

    card and deck classes added

--- card_game.py
+++ card_game.py
@@ -1,3 +1,38 @@
 #code for playing a war card game
 
+import random
 
+
+values = {'Two': 2, 'Three': 3, 'Four': 4, 'Five': 5, 'Six': 6, 'Seven': 7, 'Eight': 8, 'Nine': 9, 'Ten': 10, 'Jack': 11, 'Queen': 12, 'King':13, 'Ace': 14} 
+suits = ('Hearts','Diamonds','Spades','Clubs')
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+
+
+class Card():
+	
+	def __init__(self,suit,rank):
+		self.suit = suit
+		self.rank = rank
+		self.value = values[rank]
+
+
+	def __str__(self):
+		return self.rank + " of " + self.suit
+
+
+#three_of_clubs = Card("Clubs", "Three")
+#print(three_of_clubs.value)
+
+class Deck():
+	
+	def __init__(self):
+		self.all_cards = []
+		for suit in suits:
+			for rank in ranks:
+				#Create the Card object
+				created_card = Card(suit,rank)
+				self.all_cards.append(created_card)
+
+
+new_deck = Deck()
+print(new_deck.all_cards)
